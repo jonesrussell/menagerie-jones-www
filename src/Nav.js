ebeb9ded924055f926a6dc0af6b0d625ec402569
Russell Jones <jonesrussell42@gmail.com>
2018-01-20 13:58:07 -0500

    Fixed build, site navigation works

--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-// The Nav creates links that can be used to navigate
-// between routes.
 const Nav = () => (
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="index.html">Menagerie Jones</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div className="container">
+        <a className="navbar-brand" href="index.html">Menagerie Jones</a>
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
+        <div className="collapse navbar-collapse" id="navbarResponsive">
 			<ul className="navbar-nav ml-auto">
 				<li className="nav-item"><Link className='nav-link' to='/'>Home</Link></li>
 				<li className="nav-item"><Link className='nav-link' to='/about'>About</Link></li>
--- src/index.js
+++ src/index.js
@@ -2,19 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Nav from './Nav';
-import App from './App';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 import registerServiceWorker from './registerServiceWorker';
 //import 'jquery/dist/jquery.min.js';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render((
 	<BrowserRouter>
-		<Nav />
+		<div>
+			<Nav />
+			<Header />
+			<div className="container">
+				<Main />
+			</div>
+			<Footer />	
+		</div>
 	</BrowserRouter>
-), document.getElementById('navbarResponsive'));
-
-ReactDOM.render((
-		<App />
-), document.getElementById('root'));
+), document.getElementById('body-container'));
 
 registerServiceWorker();
