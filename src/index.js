928618660712b452b665b6456d7fd59321311880
Russell Jones <jonesrussell42@gmail.com>
2018-01-20 12:17:56 -0500

    Add multiple pages, navigation, content: broken build

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import Nav from './Nav';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'jquery/dist/jquery.min.js';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+	<BrowserRouter>
+		<Nav />
+	</BrowserRouter>
+), document.getElementById('navbarResponsive'));
+
+ReactDOM.render((
+		<App />
+), document.getElementById('root'));
+
 registerServiceWorker();
