adb1522d5eedb7377acd29797e7cd6a54d3e4d77
Russell Jones <jonesrussell42@gmail.com>
2018-01-20 21:25:10 -0500

    Fetch about and portfolio from Drupal

--- src/About.js
+++ src/About.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
+import $ from 'jquery'
 
-const About = () => (
-  <div>
-    <ul>
-      <li>6/5 @ Evergreens</li>
-      <li>6/8 vs Kickers</li>
-      <li>6/14 @ United</li>
-    </ul>
-  </div>
-)
+class About extends Component {
+	constructor() {
+		super();
+		this.state = {
+			title: '',
+			body: '',
+		};
+	};
+
+	componentDidMount() {
+		fetch('http://menagerie-jones.com/node/1?_format=json')
+			.then(results => {
+				return results.json();
+			})
+				.then(data => {
+				let title = data.title[0].value;
+				let body = $(data.body[0].value).html();
+				this.setState({ title: title, body: body });
+			});
+	}
+
+	render() {
+		return (
+			<div id="page-about">
+				<h1>{this.state.title}</h1>
+				{this.state.body}
+			</div>
+		)
+	}
+}
 
 export default About 
-
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Home = () => (
-  <div>
-    <h1>Welcome to the Tornadoes Website!</h1>
-  </div>
+	<div>
+		<h1>Hi, my name is Russell Jones, I am a Software Developer / IT Manager.</h1>
+		<button>View my work</button>
+	</div>
 )
 
 export default Home
