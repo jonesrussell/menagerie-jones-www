198cba71dfa64ce20f4af508e6d626644a7968bc
Russell Jones <jonesrussell42@gmail.com>
2018-01-23 23:21:31 -0500

    Retrieving and rendering content from API

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-//import $ from 'jquery'
+import $ from 'jquery'
 
 class Projects extends Component {
 	constructor() {
@@ -7,19 +7,40 @@ class Projects extends Component {
 		this.state = {
 			title: '',
 			body: '',
+			data: []
 		};
 	};
 
 	componentDidMount() {
+		let _this = this;
+
+		fetch('http://menagerie-jones.com/node/3?_format=json')
+			.then(results => {
+				return results.json();
+			})
+			.then(data => {
+					let title = data.title[0].value;
+					let body = '';
+
+					if (typeof data.body[0] !== 'undefined') {
+						body = $(data.body[0].value).html(); 
+					}
+
+					_this.setState({ title: title, body: body });
+			});
+
+
 		fetch('http://menagerie-jones.com/projects?_format=json')
 			.then(results => {
 				return results.json();
 			})
 				.then(data => {
-					console.log(data);
-/*				let title = data.title[0].value;
-				let body = $(data.body[0].value).html();
-				this.setState({ title: title, body: body });*/
+					_this.setState({ data: data });
+					console.log(_this.state.data);
+					_this.state.data.map(function(bar) {
+						console.log(bar);
+						return bar;
+					});
 			});
 	}
 
@@ -28,15 +49,27 @@ class Projects extends Component {
 			<div id="page-projects">
 				<h1>{this.state.title}</h1>
 				<hr/>
-				{this.state.body}
-				<div className="row">
-					<div className="col-md-7" style={{ overflow: 'hidden' }}><img src="http://placehold.it/700x300" alt="alt text"/></div>
-					<div className="col-md-5">
-						<h3>Project One</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-						<button className="btn btn-primary">Details</button>
-					</div>
+
+				<div id="projects-page-body">
+					{this.state.body}
 				</div>
+
+				{
+					this.state.data.map(function(project) {
+						return (
+							<div className="row" key={project.nid[0].value}>
+								<div className="col-md-7" style={{ overflow: 'hidden' }}>
+									<img src={project.field_image[0].url} alt={project.field_image[0].alt}/>
+								</div>
+								<div className="col-md-5">
+									<h3>{project.title[0].value}</h3>
+									<p>{$(project.body[0].value).html()}</p>
+									<button className="btn btn-primary">Details</button>
+								</div>
+							</div>
+						)
+					})
+				}
 			</div>
 		)
 	}
