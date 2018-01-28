e33d5195ffcb5d4713c1c77672cb3a5c29cda634
Russell Jones <jonesrussell42@gmail.com>
2018-01-27 21:21:12 -0500

    cleaned up projects code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Badge } from 'reactstrap';
 import $ from 'jquery';
 import './Projects.scss';
-import Lightbox from "react-image-lightbox";
+import Lightbox from 'react-image-lightbox';
 
 class Projects extends Component {
 	constructor(props) {
@@ -50,6 +50,28 @@ class Projects extends Component {
 			});
 	}
 
+	showProject(project, images, projects) {
+		let badge = (project.field_proof_of_concept[0].value) ? <Badge>Proof of concept</Badge> : '';
+		let body = <div dangerouslySetInnerHTML={{__html: project.body[0].value}} />;
+		images.push(project.field_image[0].url);
+		let imagesLength = images.length;
+
+		return (
+			<div className="row" key={project.nid[0].value}>
+				<div className="col-md-7" style={{ overflow: 'hidden' }}>
+					<img src={project.field_image[0].url} alt={project.field_image[0].alt} onClick={ function() {
+						projects.setState({ photoIndex: imagesLength - 1, isOpen: true })
+					}}/>
+				</div>
+				<div className="col-md-5">
+					<h3>{project.title[0].value} {badge}</h3>
+					<div>{body}</div>
+					<button className="btn btn-primary">Learn more</button>
+				</div>
+			</div>
+		)
+	}
+
 	render() {
 		let _this = this;
 		let isOpen = this.state.isOpen;
@@ -59,48 +81,16 @@ class Projects extends Component {
 			<div id="page-projects">
 				<h1>{this.state.title}</h1>
 				<hr/>
-
 				<div id="projects-page-body">
 					{this.state.body}
 				</div>
-
-				{
-					this.state.data.map(function(project)
-					{
-						let badge = (project.field_proof_of_concept[0].value) ? <Badge>Proof of concept</Badge> : '';
-						let body = <div dangerouslySetInnerHTML={{__html: project.body[0].value}} />;
-						images.push(project.field_image[0].url);
-//						console.log(project.field_image[0].url);
-//						console.log('i-1: ', i-1);
-						let imagesLength = images.length;
-
-						return (
-							<div className="row" key={project.nid[0].value}>
-								<div className="col-md-7" style={{ overflow: 'hidden' }}>
-									<img src={project.field_image[0].url} alt={project.field_image[0].alt} onClick={ function() {
-										console.log('imagesLength: ', imagesLength);
-										_this.setState({ photoIndex: imagesLength - 1, isOpen: true })
-									}}/>
-								</div>
-								<div className="col-md-5">
-									<h3>{project.title[0].value} {badge}</h3>
-									<div>{body}</div>
-									<button className="btn btn-primary">Learn more</button>
-								</div>
-							</div>
-						)
-
-					})
-				}
-
-			{console.log('this.state.photoIndex', this.state.photoIndex)}
+				{ this.state.data.map(function(project) { return ( _this.showProject(project, images, _this) ) }) } 
 				{isOpen && (
 				  <Lightbox
 					mainSrc={images[this.state.photoIndex]}
 					onCloseRequest={() => _this.setState({ isOpen: false })}
 				  />
 				)}
-
 			</div>
 		)
 	}
