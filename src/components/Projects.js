cf29599c81c748b4765a3229443da1afa312e990
Russell Jones <jonesrussell42@gmail.com>
2018-01-28 00:08:52 -0500

    Mobile first on homepage

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -58,12 +58,12 @@ class Projects extends Component {
 
 		return (
 			<div className="row" key={project.nid[0].value}>
-				<div className="col-md-7" style={{ overflow: 'hidden' }}>
+				<div className="col col-md-7" style={{ overflow: 'hidden' }}>
 					<img src={project.field_image[0].url} alt={project.field_image[0].alt} onClick={ function() {
 						projects.setState({ photoIndex: imagesLength - 1, isOpen: true })
 					}}/>
 				</div>
-				<div className="col-md-5">
+				<div className="col col-md-5">
 					<h3>{project.title[0].value} {badge}</h3>
 					<div>{body}</div>
 					<button className="btn btn-primary">Learn more</button>
