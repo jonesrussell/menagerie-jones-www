bc90d1122a18dd8e238edde375be24696f893c50
Russell Jones <jonesrussell42@gmail.com>
2018-01-28 20:08:42 -0500

    Theming project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,8 +20,8 @@ class Projects extends Component {
 	componentDidMount() {
 		let _this = this;
 
-		let host = 'http://live-menagerie-jones.pantheonsite.io';
-		//let host = 'http://menagerie-jones.com';
+//		let host = 'http://live-menagerie-jones.pantheonsite.io';
+		let host = 'http://192.168.0.116';
 		fetch(host + '/node/3?_format=json')
 			.then(results => {
 				return results.json();
@@ -57,17 +57,20 @@ class Projects extends Component {
 		let imagesLength = images.length;
 
 		return (
-			<div className="row" key={project.nid[0].value}>
-				<div className="col col-md-7" style={{ overflow: 'hidden' }}>
-					<img src={project.field_image[0].url} alt={project.field_image[0].alt} onClick={ function() {
-						projects.setState({ photoIndex: imagesLength - 1, isOpen: true })
-					}}/>
+			<div>
+				<div className="row project-container" key={project.nid[0].value}>
+					<div className="col col-md-7" style={{ overflow: 'hidden' }}>
+						<img src={project.field_image[0].url} alt={project.field_image[0].alt} onClick={ function() {
+							projects.setState({ photoIndex: imagesLength - 1, isOpen: true })
+						}}/>
+					</div>
+					<div className="col col-md-5">
+						<h3>{project.title[0].value} {badge}</h3>
+						<div>{body}</div>
+						<strong class="source">Source: </strong> <i className="fa fa-github-square fa-3x" aria-hidden="true"></i>
+					</div>
 				</div>
-				<div className="col col-md-5">
-					<h3>{project.title[0].value} {badge}</h3>
-					<div>{body}</div>
-					<button className="btn btn-primary">Learn more</button>
-				</div>
+				<hr/>
 			</div>
 		)
 	}
