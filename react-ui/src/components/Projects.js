10b79eaddae8233503257f9f601b47d07004ec59
Russell Jones <jonesrussell42@gmail.com>
2018-01-30 17:00:28 -0500

    Cleaned up projects code

--- react-ui/src/components/Projects.js
+++ react-ui/src/components/Projects.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Badge } from 'reactstrap';
-import $ from 'jquery';
 import Lightbox from 'react-image-lightbox';
 import './Projects.scss';
 
@@ -13,12 +12,14 @@ class Projects extends Component {
 			body: '',
 			projects: [],
 			isOpen: false,
-			images: []
+			images: [],
+			photoIndex: 0
 		};
 	};
 
 	componentDidMount() {
 		let _this = this;
+
 		fetch(process.env.REACT_APP_API_URL + '/node/3?_format=json')
 			.then(results => {
 				return results.json();
@@ -28,41 +29,74 @@ class Projects extends Component {
 					let body = '';
 
 					if (typeof data.body[0] !== 'undefined') {
-						body = $(data.body[0].value).html(); 
+						body = data.body[0].value; 
 					}
 
-					_this.setState({ title: title, body: body });
+					_this.setState({
+						title: title,
+						body: body
+					});
 			});
 
 		fetch(process.env.REACT_APP_API_URL + '/projects?_format=json')
 			.then(results => {
 				return results.json();
 			})
-				.then(data => {
-					_this.setState({ projects: data });
+			.then(data => {
+				_this.setState({ projects: data });
 			});
 	}
 
-	showProject(project, images, projects) {
-		let badge = (project.field_proof_of_concept[0].value) ? <Badge>Proof of concept</Badge> : '';
-		let body = <div dangerouslySetInnerHTML={{__html: project.body[0].value}} />;
+	showImage(image, i) {
+		let _this = this;
+
+		return <img src={image[0].url} alt={image[0].alt} onClick={ function() {
+			_this.setState({
+				photoIndex: i,
+				isOpen: true
+			})
+		}}/>
+	}
+
+	showBadge(badge) {
+		return (badge[0].value) 
+			? <Badge>Proof of concept</Badge>
+			: '';
+	}
+
+	showDemo(demo) {
+		return (typeof demo[0] !== 'undefined') ?
+			<div className="demo">
+				<strong>Demo:</strong>
+				<a href={demo[0].uri} target="_blank">{demo[0].uri}</a>
+			</div> : '';
+	}
+
+	showGithub(github) {
+		return (typeof github[0] !== 'undefined') ?
+			<div className="github">
+				<strong className="source">Source:</strong>{
+					// eslint-disable-next-line
+				 } <a href={github[0].uri} target="_blank">
+					<i className="fa fa-github-square fa-3x" aria-hidden="true"></i>
+				</a>
+			</div> : '';
+	}
+
+	showProject(project, images) {
 		images.push(project.field_image[0].url);
-		let imagesLength = images.length;
 
 		return (
 			<div key={project.nid[0].value} id="projects">
 				<div className="row project-container">
 					<div className="col col-md-7" style={{ overflow: 'hidden' }}>
-						<img src={project.field_image[0].url} alt={project.field_image[0].alt} onClick={ function() {
-							projects.setState({ photoIndex: imagesLength - 1, isOpen: true })
-						}}/>
+						{this.showImage(project.field_image, images.length - 1)}
 					</div>
 					<div className="col col-md-5">
-						<h3>{project.title[0].value} {badge}</h3>
-						<div>{body}</div>
-						<strong className="source">Source:</strong>{
-							// eslint-disable-next-line
-						 } <a href={project.field_github[0].uri} target="_blank"><i className="fa fa-github-square fa-3x" aria-hidden="true"></i></a>
+						<h3>{project.title[0].value} {this.showBadge(project.field_proof_of_concept)}</h3>
+						<div dangerouslySetInnerHTML={{__html: project.body[0].value}} />
+						{this.showDemo(project.field_demo)}
+						{this.showGithub(project.field_github)}
 					</div>
 				</div>
 				<hr/>
@@ -81,7 +115,9 @@ class Projects extends Component {
 				<div id="projects-page-body">
 					{this.state.body}
 				</div>
-				{ this.state.projects.map(function(project) { return ( _this.showProject(project, images, _this) ) }) } 
+				{this.state.projects.map(function(project) {
+					return ( _this.showProject(project, images) )
+				})} 
 				{this.state.isOpen && (
 				  <Lightbox
 					mainSrc={images[this.state.photoIndex]}
